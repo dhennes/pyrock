a2d807a195dadf9a8da7bc89a40ba3e52f4f554f
Daniel Hennes <daniel.hennes@gmail.com>
2016-07-01 16:40:20 +0200

    improved task model for pyrock display

--- examples/display.py
+++ examples/display.py
@@ -1,64 +1,238 @@
 import sys
+
 from PySide.QtGui import *
+from PySide.QtCore import QThread
+
+import PySide.QtGui as QtGui
+import PySide.QtCore as QtCore
+
+from time import sleep
 
 import pyrock
 
 
-app = QApplication(sys.argv)
+class TaskModel(QtCore.QAbstractItemModel):
 
-tree_view = QTreeView()
-tree_view.setWindowTitle('PyRock TaskInspector')
-tree_view.setMinimumSize(600, 400)
-model = QStandardItemModel(tree_view)
-tree_view.setModel(model)
+    HEADERS = ('Name', 'Value')
+    ICON_IN  = QIcon.fromTheme('go-next')
+    ICON_OUT = QIcon.fromTheme('go-previous')
+    ICON_START  = QIcon.fromTheme('media-playback-start')
+    ICON_STOP = QIcon.fromTheme('process-stop')
+    ICON_CONFIGURE = QIcon.fromTheme('preferences-system')
+    ICON_CLEANUP = QIcon.fromTheme('view-refresh')
 
-model.setHorizontalHeaderLabels(('Name', 'Type'))
-tree_view.setUniformRowHeights(True)
+    def __init__(self, tasks):
+        QtCore.QAbstractItemModel.__init__(self)
+        self.tasks = tasks
 
-tree_view.setEditTriggers(QAbstractItemView.NoEditTriggers)
-icon_in = QIcon.fromTheme('go-next')
-icon_out = QIcon.fromTheme('go-previous')
+    def columnCount(self, parent):
+        return 2
 
-proxies = map(pyrock.TaskProxy, pyrock.nameservice.names())
-for proxy in proxies:
-    task_item = QStandardItem(proxy.name)
-    for name, type_name in proxy.output_ports.iteritems():
-        print name, type_name
-        item_name = QStandardItem(name)
-        item_name.setIcon(icon_out)
-        item_type = QStandardItem(type_name)
-        task_item.appendRow((item_name, item_type))
-    for name, type_name in proxy.input_ports.iteritems():
-        print name, type_name
-        item_name = QStandardItem(name)
-        item_name.setIcon(icon_in)
-        item_type = QStandardItem(type_name)
-        task_item.appendRow((item_name, item_type))
+    def rowCount(self, parent):
+        if not parent.isValid():
+            return len(self.tasks)
+        node = parent.internalPointer()
+        if isinstance(node, pyrock.TaskProxy):
+            return len(node.ports)
+        return 0
 
-    if proxy.is_connected():
-        if proxy.is_running():
-            status_item = QStandardItem('running')
-        elif proxy.is_configured():
-            status_item = QStandardItem('stopped')
+    def data(self, index, role):
+        if not index.isValid():
+            return None
+
+        node = index.internalPointer()
+        if role == QtCore.Qt.DisplayRole:
+            if isinstance(node, pyrock.TaskProxy):
+                if index.column() == 0:
+                    return node.name
+                if index.column() == 1:
+                    return str(node.state())
+            
+            if isinstance(node, pyrock.Port):
+                if index.column() == 0:
+                    return node.name
+                if index.column() == 1:
+                    return node.type_name
+
+        if role == QtCore.Qt.DecorationRole and isinstance(node, pyrock.Port) and index.column() == 0:
+            if node.port_type == pyrock.Port.COutput:
+                return self.ICON_OUT
+            else:
+                return self.ICON_IN
+            
+    def headerData(self, column, orientation, role):
+        if (orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole):
+            return self.HEADERS[column]
+
+    def index(self, row, column, parent=None):
+        if not self.hasIndex(row, column, parent):
+            return QtCore.QModelIndex()
+
+        if not parent.isValid():
+            return self.createIndex(row, column, self.tasks[row])
+
+        task = parent.internalPointer()
+        return self.createIndex(row, column, task.ports[row])
+
+    def parent(self, index):
+        if not index.isValid():
+            return QtCore.QModelIndex()
+        node = index.internalPointer()
+        if isinstance(node, pyrock.Port):
+            return self.createIndex(self.tasks.index(node.task), 0, node.task)
         else:
-            status_item = QStandardItem('pre-configured')
-    else:
-        status_item = QStandardItem('not connected')
-        task_item.setEnabled(False)
-        status_item.setEnabled(False)
-    model.appendRow((task_item, status_item))
-#index = model.indexFromItem(task_item)
-#tree_view.expandAll()
-#tree_view.setCurrentIndex(index)
-tree_view.show()
-
-#table.verticalHeader().hide()
-
-tree_view.header().setResizeMode(QHeaderView.ResizeToContents)
-# enable sorting
-#table.setSortingEnabled(True)
+            return QtCore.QModelIndex()
 
 
-# Enter Qt application main loop
-app.exec_()
-sys.exit()
+class FilterProxyModel(QSortFilterProxyModel):
+    def __init__(self):
+        super(FilterProxyModel, self).__init__()
+
+    def filterAcceptsColumn(self, column, parent):
+        return True
+    
+    def filterAcceptsRow(self, row, parent):
+        idx = self.sourceModel().index(row, 0, parent)
+        if idx.isValid() and not idx.parent().isValid(): # check for top level entry aka Task
+            task = idx.internalPointer()
+            if '_Logger' in task.name:
+                return False
+        return True
+
+    def lessThan(self, left, right):
+        return self.sourceModel().data(left, QtCore.Qt.DisplayRole) < self.sourceModel().data(right, QtCore.Qt.DisplayRole)
+
+
+class TaskTreeView(QtGui.QTreeView):
+
+    REFRESH_TIMEOUT = 100
+    ICON_IN  = QIcon.fromTheme('go-next')
+    ICON_OUT = QIcon.fromTheme('go-previous')
+    ICON_START  = QIcon.fromTheme('media-playback-start')
+    ICON_STOP = QIcon.fromTheme('process-stop')
+    ICON_CONFIGURE = QIcon.fromTheme('preferences-system')
+    ICON_CLEANUP = QIcon.fromTheme('view-refresh')
+
+    def __init__(self, parent=None):
+        super(TaskTreeView, self).__init__(parent)
+
+        # menu = QtGui.QMenu("menu", self)
+        # menu.addAction(QtGui.QAction('50%', menu, checkable=True))
+        # self.parent.menuBar().addMenu(menu)
+
+
+        self.setWindowTitle('PyRock TaskInspector')
+        self.setMinimumSize(600, 400)
+        self.setEditTriggers(QtGui.QAbstractItemView.NoEditTriggers)
+        # self.header().setStretchLastSection(False)
+        # self.header().setResizeMode(1, QHeaderView.Interactive)
+        # self.header().setResizeMode(QHeaderView.ResizeToContents)
+
+        self.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.open_menu)
+        self.setAlternatingRowColors(True)
+
+        self.tasks = map(pyrock.TaskProxy, pyrock.nameservice.names())
+        self.model = TaskModel(self.tasks)
+
+        self.proxy_model = FilterProxyModel()
+        self.proxy_model.setSourceModel(self.model)
+
+        self.setModel(self.proxy_model)
+
+        self.header().setResizeMode(0, QHeaderView.ResizeToContents)
+        self.setSortingEnabled(True)
+
+        # setup refresh timer
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.refresh)
+        self.timer.start(TaskTreeView.REFRESH_TIMEOUT)
+
+
+    def open_menu(self, point):
+        index = self.indexAt(point)
+        if index.isValid() and not index.parent().isValid(): # check for top level rows
+            index = self.proxy_model.mapToSource(index)
+            menu = QMenu()
+            proxy = index.internalPointer()
+            if proxy.is_configured():
+                if proxy.is_running():
+                    action = menu.addAction(TaskTreeView.ICON_STOP, 'stop', proxy.stop).setIconVisibleInMenu(True)
+                    #action.setEnabled(False)
+                else:
+                    item = menu.addAction(TaskTreeView.ICON_START, 'start', proxy.start).setIconVisibleInMenu(True)
+                    menu.addAction(TaskTreeView.ICON_CLEANUP, 'cleanup', proxy.cleanup).setIconVisibleInMenu(True)
+            else:
+                menu.addAction(TaskTreeView.ICON_CONFIGURE, 'configure', proxy.configure).setIconVisibleInMenu(True)
+
+            menu.exec_(self.viewport().mapToGlobal(point))        
+
+
+    def refresh(self):
+        task_names = set(pyrock.nameservice.names())
+        proxy_names = set([p.name for p in self.model.tasks])
+
+        if len(task_names) == len(proxy_names) == len(task_names & proxy_names):
+            return
+   
+        # delete tasks
+        for task_name in proxy_names - task_names:
+            idx = [p.name for p in self.model.tasks].index(task_name)
+            self.model.beginRemoveRows(QtCore.QModelIndex(), idx, idx)
+            del self.model.tasks[idx]
+            self.model.endRemoveRows()
+
+        # add tasks
+        for task_name in task_names - proxy_names:
+            proxy = pyrock.TaskProxy(task_name)
+            idx = len(self.model.tasks)
+            self.model.beginInsertRows(QtCore.QModelIndex(), idx, idx)
+            self.model.tasks.append(proxy)
+            self.model.endInsertRows()
+
+
+class PyRockDisplay(QtGui.QMainWindow):
+    def __init__(self):
+        QtGui.QMainWindow.__init__(self)
+        self.treeview = TaskTreeView()
+        self.setCentralWidget(self.treeview)
+
+        # self.menu = QtGui.QMenu('View', self)
+        # self.menu.addAction(QtGui.QAction('filter Loggers', self.menu, checkable=True))
+        # self.menuBar().addMenu(self.menu)
+
+        self.toolbar = QtGui.QToolBar('', self)
+        self.toolbar.setMovable(False)
+
+        self.filter_action = QtGui.QAction('Loggers', self.toolbar, checkable=True, checked=True)
+        # self.filter_action.triggered.connect(self.foo)
+
+        self.lineedit = QtGui.QLineEdit('', self.toolbar, placeholderText='filter...')
+
+        self.toolbar.addWidget(self.lineedit)
+        self.toolbar.addAction(self.filter_action)
+
+
+        # self.check = QtGui.QCheckBox('filter &loggers', self.toolbar)
+        # self.toolbar.addWidget(self.check)
+
+        self.addToolBar(self.toolbar)
+
+        self.connect(QtGui.QShortcut(QtGui.QKeySequence('Ctrl+F'), self), QtCore.SIGNAL('activated()'), self.activate_lineedit)
+#        self.connect(QtGui.QShortcut(QtGui.QKeySequence('Ctrl+C'), self), QtCore.SIGNAL('activated()'), self.close)
+
+
+    def activate_lineedit(self):
+        self.lineedit.selectAll()
+        self.lineedit.setFocus()
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+
+    window = PyRockDisplay()
+    window.show()
+
+    # treeview = TaskTreeView()
+    # treeview.show()
+
+    sys.exit(app.exec_())
--- pyrock/proxy.py
+++ pyrock/proxy.py
@@ -4,7 +4,20 @@ import sys
 from collections import deque
 
 
-class TaskProxy:
+
+class Port(object):
+
+    COutput = pyrock.RTT.corba.COutput
+    CInput = pyrock.RTT.corba.CInput
+
+    def __init__(self, task, name, type_name, port_type):
+        self.task = task
+        self.name = name
+        self.type_name = type_name
+        self.port_type = port_type
+ 
+
+class TaskProxy(object):
 
     def __init__(self, taskname):
         self.name = taskname
@@ -14,10 +27,15 @@ class TaskProxy:
         except Exception as e:
             print(self.name, e)
             desc = {}
+        
+        self.ports = [Port(self, p.name, p.type_name, p.type) for p in desc]
+            
+        # TODO: remove
         self.output_ports = {
             p.name: p.type_name for p in desc if p.type is pyrock.RTT.corba.COutput}
         self.input_ports = {
             p.name: p.type_name for p in desc if p.type is pyrock.RTT.corba.CInput}
+
         self.subscriptions = deque()
 
     def subscripe(self, portname, callback, policy=pyrock.create_default_policy()):
@@ -35,26 +53,26 @@ class TaskProxy:
             return False
         return True
 
-    def state(self):
-        return self._task.getTaskState()
-
-    def cleanup(self):
-        self._task.cleanup()
-
-    def configure(self):
-        self._task.configure()
-
     def is_configured(self):
         return self._task.isConfigured()
 
-    def start(self):
-        self._task.start()
-
-    def stop(self):
-        self._task.stop()
-
     def is_running(self):
         return self._task.isRunning()
+
+    def state(self):
+        return self._task.getTaskState()
+
+    def configure(self):
+        return self._task.configure()
+
+    def cleanup(self):
+        return self._task.cleanup()
+
+    def start(self):
+        return self._task.start()
+
+    def stop(self):
+        return self._task.stop()
 
     def spin_once(self):
         sub = self.subscriptions.popleft()
