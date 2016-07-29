bbe61cc6cac9be04a0537c128b28e963a8c32eec
Daniel Hennes <daniel.hennes@gmail.com>
2016-07-29 12:03:53 +0200

    catch NamingContext.NotFound

--- pyrock/nameservice.py
+++ pyrock/nameservice.py
@@ -17,13 +17,29 @@ class NameService():
     def __init__(self, orb, namespace='TaskContexts'):
         self.orb = orb
         self.namespace = namespace
-        root_context = self.orb.resolve_initial_references(
-            'NameService')._narrow(CosNaming.NamingContext)
-        self.context = root_context.resolve(_str2nc(namespace))
+        self.context = self._resolve_context(self.namespace)
+
+    def _resolve_context(self, namespace):
+        context = None
+        try:
+            root_context = self.orb.resolve_initial_references(
+                'NameService')._narrow(CosNaming.NamingContext)
+            context = root_context.resolve(_str2nc(namespace))
+        except CosNaming.NamingContext.NotFound:
+            pass
+        return context
 
     def names(self):
+        if not self.context:
+            self.context = self._resolve_context(self.namespace)
+        if not self.context:
+            return []
         blist, _ = self.context.list(_MAX_NAMES)
         return [_nc2str(binding.binding_name) for binding in blist]
 
     def get(self, name):
+        if not self.context:
+            self.context = self._resolve_context(self.namespace)
+        if not self.context:
+            return None
         return self.context.resolve(_str2nc(name))
